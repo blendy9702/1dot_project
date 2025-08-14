21c4a876b68bd42028c843c25c17352ffde2a09f
blendy9702 <lch99206@naver.com>
2025-08-14 20:47:23 +0900

    chart done

--- src/app/components/EditForm.tsx
+++ src/app/components/EditForm.tsx
@@ -7,42 +7,46 @@ type EditFormProps = {
 
 export function EditForm({ editRow, onChange }: EditFormProps) {
   return (
-    <div className="row g-3">
-      <div className="col-12 col-md-4">
-        <label className="form-label">구분</label>
-        <select
-          className="form-select"
-          value={editRow.section}
-          onChange={(e) => onChange({ ...editRow, section: e.target.value })}
-        >
-          <option value="월보장">월보장</option>
-          <option value="미분류">미분류</option>
-          <option value="슬롯">슬롯</option>
-        </select>
+    <div className="d-flex flex-column gap-3">
+      <div className="d-flex gap-3 justify-content-center align-items-center">
+        <div className="col-12 col-md-6">
+          <label className="form-label">구분</label>
+          <select
+            className="form-select"
+            value={editRow.section}
+            onChange={(e) => onChange({ ...editRow, section: e.target.value })}
+          >
+            <option value="월보장">월보장</option>
+            <option value="미분류">미분류</option>
+            <option value="슬롯">슬롯</option>
+          </select>
+        </div>
+        <div className="col-12 col-md-6">
+          <label className="form-label">PID</label>
+          <input
+            className="form-control"
+            value={editRow.pid}
+            onChange={(e) => onChange({ ...editRow, pid: e.target.value })}
+          />
+        </div>
       </div>
-      <div className="col-12 col-md-4">
-        <label className="form-label">PID</label>
-        <input
-          className="form-control"
-          value={editRow.pid}
-          onChange={(e) => onChange({ ...editRow, pid: e.target.value })}
-        />
-      </div>
-      <div className="col-12 col-md-6">
-        <label className="form-label">키워드</label>
-        <input
-          className="form-control"
-          value={editRow.keyword}
-          onChange={(e) => onChange({ ...editRow, keyword: e.target.value })}
-        />
-      </div>
-      <div className="col-12 col-md-6">
-        <label className="form-label">플레이스</label>
-        <input
-          className="form-control"
-          value={editRow.place}
-          onChange={(e) => onChange({ ...editRow, place: e.target.value })}
-        />
+      <div className="d-flex gap-3 justify-content-center align-items-center">
+        <div className="col-12 col-md-6">
+          <label className="form-label">키워드</label>
+          <input
+            className="form-control"
+            value={editRow.keyword}
+            onChange={(e) => onChange({ ...editRow, keyword: e.target.value })}
+          />
+        </div>
+        <div className="col-12 col-md-6">
+          <label className="form-label">플레이스</label>
+          <input
+            className="form-control"
+            value={editRow.place}
+            onChange={(e) => onChange({ ...editRow, place: e.target.value })}
+          />
+        </div>
       </div>
     </div>
   );
